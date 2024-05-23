7a90a8cf008e937822e95b6b4735cb784662a099
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-05-23 09:29:19 +0100

    Header of the page

--- ATA-Front-Office/src/Layouts/Header.jsx
+++ ATA-Front-Office/src/Layouts/Header.jsx
@@ -1,35 +1,46 @@
 import {Link} from "react-router-dom"
+import ATALogo from "../assets/Images/ATALogo"
 
 export default function Header(){
     return(
         <>
-        <nav class="flex items-center justify-between flex-wrap bg-slate-900 h-2">
-            <div class="flex items-center flex-shrink-0 text-white mr-6">
-                <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-                <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
-            </div>
-            <div class="block lg:hidden">
-                <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                </button>
-            </div>
-            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                <div class="text-sm lg:flex-grow">
-                <Link to="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                    Docs
-                </Link>
-                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                    Examples
-                </a>
-                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-                    Blog
-                </a>
+
+
+                <nav className="flex items-center justify-between flex-wrap bg-slate-900 px-36 py-5 border-b-2 border-white">
+                <div className="flex items-center flex-shrink-0 text-white mr-6 px-1.5">
+                    <ATALogo/>
                 </div>
-                <div>
-                <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+                <div className="block lg:hidden">
+                    <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                    </button>
                 </div>
-            </div>
-            </nav>
+                <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                    <div className="text-sm lg:flex-grow lg:flex  lg:justify-center lg:ml-36 ">
+                    <a href="#responsive-header" className="block mt-4 text-lg lg:inline-block lg:mt-0 text-white hover:text-white mr-4 font-semibold">
+                        L’école
+                    </a>
+                    <a href="#responsive-header" className="block mt-4 text-lg lg:inline-block lg:mt-0 text-white hover:text-white mr-4 font-semibold">
+                        Formations
+                    </a>
+                    <a href="#responsive-header" className="block mt-4 text-lg lg:inline-block lg:mt-0 text-white hover:text-white font-semibold">
+                        Financements
+                    </a>
+                    </div>
+                    {/* <div className="flex-shrink-0">
+                    <a href="#" className="inline-block px-8 text-lg py-3 leading-none border-2 border-gradient-to-r from-cyan-500 to-blue-500  rounded text-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-2 lg:mt-0 lg:mb-2 font-semibold">Nous Contacter</a>
+                    </div> */}
+                    <div className="flex-shrink-0">
+                        <div className="relative inline-block p-0.5 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg">
+                            <a href="#" className="block px-8 text-lg py-3 leading-none bg-slate-900 text-white rounded-md hover:text-teal-500 hover:bg-white font-semibold">
+                            Nous Contacter
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+                </nav>
+ 
         
         </>
     )
