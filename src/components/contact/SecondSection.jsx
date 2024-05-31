b6f4031b1f280540072b72d39b1ff570f1d48ed7
Mohamed072005 <amin072005@gmail.com>
2024-05-31 15:43:23 +0100

    create the calendar of the reservation in contact page

--- src/components/contact/SecondSection.jsx
+++ src/components/contact/SecondSection.jsx
@@ -1,9 +1,10 @@
 export default function SecondSection() {
     return (
         <>
-            <div className="">
-                <div className="text-white">
-                    <h2>Se faire rappeler pour échanger sur votre projet et répondre à vos questions</h2>
+            <div className="relative">
+                <div className="contact-text-background"></div>
+                <div className="contact-text-container">
+                    <h2 className="text-white text-3xl font-bold">Se faire rappeler pour échanger sur votre projet et répondre à vos questions</h2>
                 </div>
             </div>
         </>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,13 @@
 import FooterPartenaires from "../components/footer/FooterPartenaires"
 import FirstSection from "../components/contact/FirstSection"
 import SecondSection from "../components/contact/SecondSection"
+import CalendarSection from "../components/contact/CalendarSection"
 export default function Contact(){
     return(
         <>
             <FirstSection />
             <SecondSection />
+            <CalendarSection />
             <FooterPartenaires />
         </>
     )
