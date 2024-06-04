733cd8ffa3a2e64e9a03b4e1fc17dd613bd7027b
Mohamed072005 <amin072005@gmail.com>
2024-06-04 09:20:41 +0100

    add the third section in contact page , the section of the Envoyez-nous un message

---
+++ src/components/contact/ThirdSection.jsx
@@ -0,0 +1,12 @@
+export default function ThirdSection (){
+    return (
+        <>
+        <div className="relative">
+            <div className="second-contact-background"></div>
+            <div className="second-contact-background-text flex justify-center">
+                <h2 className="text-white font-bold text-3xl">Envoyez-nous<span className="text-[#39DDF5]"> un message !</span></h2>
+            </div>
+        </div>
+        </>
+    );
+}
