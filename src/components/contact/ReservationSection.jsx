85fe035362444f07427179f8c12df7b1edf524d2
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-06-05 09:57:21 +0100

    see how it works

--- src/components/contact/ReservationSection.jsx
+++ src/components/contact/ReservationSection.jsx
@@ -106,6 +106,8 @@ export default function ReservationSection() {
                         </div>
                     </div>
                 </div>
+
+                
             </>
 
         );
--- src/components/contact/ThirdSection.jsx
+++ src/components/contact/ThirdSection.jsx
@@ -1,3 +1,4 @@
+
 export default function ThirdSection (){
     return (
         <>
@@ -5,6 +6,9 @@ export default function ThirdSection (){
             <div className="second-contact-background"></div>
             <div className="second-contact-background-text flex justify-center">
                 <h2 className="text-white font-bold text-3xl">Envoyez-nous<span className="text-[#39DDF5]"> un message !</span></h2>
+            </div>
+            <div>
+
             </div>
         </div>
         </>
