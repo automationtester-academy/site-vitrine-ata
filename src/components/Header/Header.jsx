4840534717e6dfb546573cca35ce9f2dfdf066b8
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-06-05 09:59:39 +0100

    changing the link on logo to take me home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,9 +12,9 @@ export default function Header() {
     return (
         <>
             <nav className="navbar flex items-center justify-between flex-wrap bg-slate-900 p-4 lg:px-36 py-2 border-b-2 border-white">
-                <div className="navbar-logo flex items-center flex-shrink-0 text-white">
+                <Link to="/" className="navbar-logo flex items-center flex-shrink-0 text-white">
                     <ATALogo />
-                </div>
+                </Link>
                 <div className="block lg:hidden">
                     <button onClick={toggleMenu} className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
                         <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" > <title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /> </svg>
