c9dcfeb6a646224d2f388f46665a2cdcc9e99266
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-07-01 10:44:50 +0100

    The background img

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
+import notfound from '../assets/backgrounds/NotFoundBackground.png'
 export default function NotFound(){
     return(
         <>
-            hi from contact
+            <section>
+                <div className='flex justify-end'>
+                    <img src={notfound} alt="" />
+                </div>
+            </section>
         </>
     )
 }
