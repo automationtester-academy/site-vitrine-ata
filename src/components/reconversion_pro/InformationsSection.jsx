cc8f39740f3c20b2a9393e7b811243fbdabefa32
OSMaben <freegiftcardishere@gmail.com>
2024-07-02 14:12:09 +0100

    merging between the hotfix and the formation fixing the pro

--- src/components/reconversion_pro/InformationsSection.jsx
+++ src/components/reconversion_pro/InformationsSection.jsx
@@ -11,7 +11,7 @@ export default function InformationsSection(){
                 {/* <InfoPratique /> */}
                 <CadreReglementaire />
             </div>
-        </div>
+        </div>  
         </>
     )
 }
