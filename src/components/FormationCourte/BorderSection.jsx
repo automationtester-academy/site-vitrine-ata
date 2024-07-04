d99fb68a688e31ca14fe2b570531c7bddca8c5b5
Mohamed072005 <amin072005@gmail.com>
2024-07-04 14:29:14 +0100

    commit the changes before pulling

--- src/components/FormationCourte/BorderSection.jsx
+++ src/components/FormationCourte/BorderSection.jsx
@@ -8,30 +8,30 @@ export default function BorderSection() {
                 <div className="button-container">
                     <div className="flex justify-evenly items-center flex-wrap w-3/4">
                         <button
-                         className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600"                            aria-label="Section Bootcamp"
+                            className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600" aria-label="Section Bootcamp"
                         >
                             Front
                         </button>
                         <button
-                         className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600"                            aria-label="Section Formation courte"
+                            className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600" aria-label="Section Formation courte"
                         >
-                            Back 
+                            Back
                         </button>
-                        <button 
-                         className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600"                            aria-label="Section Abonnement"
+                        <button
+                            className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600" aria-label="Section Abonnement"
                         >
                             Performances
                         </button>
                         <button
-                        className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600"                            aria-label="Section Reconversion pro"
+                            className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600" aria-label="Section Reconversion pro"
                         >
-                             CI/CD
+                            CI/CD
                         </button>
                         <button
-                        className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600"                            aria-label="Section Mix-learning"
+                            className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600" aria-label="Section Mix-learning"
                         >
-                       Mobile    
-                      </button>
+                            Mobile
+                        </button>
                     </div>
                 </div>
             </div>
