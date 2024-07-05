7a6a6271fe8ce04cd0f916488967e00bfe4cd575
Basma <basmaelandalosy5@gmail.com>
2024-07-05 14:18:33 +0100

    Modification dans le border

--- src/components/FormationCourte/BorderSection.jsx
+++ src/components/FormationCourte/BorderSection.jsx
@@ -2,39 +2,42 @@ import React from "react";
 
 export default function BorderSection() {
     return (
-        <>
-            <div className="relative">
-                <div className="profils-background w-full h-full p-7"></div>
-                <div className="button-container">
-                    <div className="flex justify-evenly items-center flex-wrap w-3/4">
-                        <button
-                         className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600"                            aria-label="Section Bootcamp"
-                        >
-                            Front
-                        </button>
-                        <button
-                         className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600"                            aria-label="Section Formation courte"
-                        >
-                            Back 
-                        </button>
-                        <button 
-                         className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600"                            aria-label="Section Abonnement"
-                        >
-                            Performances
-                        </button>
-                        <button
-                        className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600"                            aria-label="Section Reconversion pro"
-                        >
-                             CI/CD
-                        </button>
-                        <button
-                        className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600"                            aria-label="Section Mix-learning"
-                        >
-                       Mobile    
-                      </button>
-                    </div>
+        <div className="relative">
+            <div className="profils-background w-full h-full p-7 absolute inset-0 z-0 pt-4"></div>
+            <div className="relative z-10">
+                <div className="flex flex-col sm:flex-row justify-evenly items-center flex-wrap w-3/4 mx-auto">
+                    <button
+                        className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600 sm:border-0 border-b border-gray-300 w-full sm:w-auto mb-2 sm:mb-0"
+                        aria-label="Section Bootcamp"
+                    >
+                        Front
+                    </button>
+                    <button
+                        className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600 sm:border-0 border-b border-gray-300 w-full sm:w-auto mb-2 sm:mb-0"
+                        aria-label="Section Formation courte"
+                    >
+                        Back
+                    </button>
+                    <button
+                        className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600 sm:border-0 border-b border-gray-300 w-full sm:w-auto mb-2 sm:mb-0"
+                        aria-label="Section Abonnement"
+                    >
+                        Performances
+                    </button>
+                    <button
+                        className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600 sm:border-0 border-b border-gray-300 w-full sm:w-auto mb-2 sm:mb-0"
+                        aria-label="Section Reconversion pro"
+                    >
+                        CI/CD
+                    </button>
+                    <button
+                        className="text-white font-bold py-2 px-4 rounded hover:bg-cyan-600 sm:border-0 border-b border-gray-300 w-full sm:w-auto mb-2 sm:mb-0"
+                        aria-label="Section Mix-learning"
+                    >
+                        Mobile
+                    </button>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
--- src/components/FormationCourte/HeroSectionFc.jsx
+++ src/components/FormationCourte/HeroSectionFc.jsx
@@ -27,8 +27,8 @@ export default function HeroSectionFc() {
         />
       ))}
      <div className="text-container absolute inset-0 flex items-center justify-center">
-  <h2 className="uppercase text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-center p-4 rounded-md">
-    <span className="text-[#39DDF5]">Formation</span> courte
+     <h2 className="uppercase text-white flex gap-3 text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold text-center p-4 rounded-md">
+     <span className="text-[#39DDF5]">Formation</span> courte
   </h2>
 </div>
     </div>
