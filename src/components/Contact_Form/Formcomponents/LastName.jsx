48a49dafc01552f10b9d6bfe09c198ec5b122761
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-08-05 11:31:00 +0100

    sending messages

--- src/components/Contact_Form/Formcomponents/LastName.jsx
+++ src/components/Contact_Form/Formcomponents/LastName.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 function LastName() {
     return (
         <div className="flex-1 px-2 mt-4 md:mt-0">
-
             <div className="relative h-11 w-full min-w-[200px]">
                 <input placeholder="" name="prenome" type={"text"}
                        className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100"/>
