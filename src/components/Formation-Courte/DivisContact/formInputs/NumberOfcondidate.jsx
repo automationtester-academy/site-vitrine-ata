ae4b42221b36e7fdf1221218e715a04169ce1cd9
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-08-06 10:13:27 +0100

    Affichage des formations courtes en utilisant Json

--- src/components/Formation-Courte/DivisContact/formInputs/NumberOfcondidate.jsx
+++ src/components/Formation-Courte/DivisContact/formInputs/NumberOfcondidate.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 function NumberOfCondidates() {
     return (
         <div className="flex-1 px-2">
-            
-
-
             <div className="relative h-11 w-full min-w-[200px]    max-sm:mb-10">
                 <label for="underline_select" class="sr-only">Underline select</label>
                 <select id="underline_select" name="NombreDeParticipant" class="text-white peer h-full w-full border-b border-blue-white bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-white outline outline-0 transition-all placeholder-shown:border-blue-white focus:border-white focus:outline-0 disabled:border-0 disabled:bg-blue-whiteplaceholder:opacity-0 focus:placeholder:opacity-100">
