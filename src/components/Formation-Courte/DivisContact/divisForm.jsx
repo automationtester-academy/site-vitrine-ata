51a4c5f1135797db9bbf92cd3fe05ffdd6ed6877
Mohamed072005 <amin072005@gmail.com>
2024-08-07 09:27:16 +0100

    commit the changes before pulling

--- src/components/Formation-Courte/DivisContact/divisForm.jsx
+++ src/components/Formation-Courte/DivisContact/divisForm.jsx
@@ -20,27 +20,24 @@ export default function DivisForm() {
     const [numberOfCondidates, setNumberOfCondidates] = useState("");
     const [startingDate, setStartingDate] = useState("");
 
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const aboutUs = `[Company Email: ${email}], [Formation souhaité: ${formation}], [Mode de formation: ${modeDeFormation}], [Date de début souhaitée: ${startingDate}]`;
-
         const formattedData = {
-            properties: [
-                { name: "name", value: companyName },
-                { name: "numberofemployees", value: numberOfCondidates },
-                { name: "about_us", value: aboutUs }
-            ]
-            // companyName,
-            // numberOfCondidates,
-            // aboutUs
+            companyName,
+            numberOfCondidates,
+            email,
+            formation,
+            modeDeFormation,
+            startingDate
         };
 
         console.log("Data to be sent:", formattedData);
 
         try {
             const response = await axios.post(
-                "https://hook.eu2.make.com/gwppk44w2vxfiebchcmnr1rc6vn79c3c",
+                "https://hook.eu2.make.com/c4oiyt4ndhw4d5jimeb2ifw1abus3s99",
                 formattedData,
                 {
                     headers: {
@@ -71,18 +68,7 @@ export default function DivisForm() {
                     <div className="lg:w-1/2 lg:mx-10">
                         <form className="relative mb-[10%] mt-6 max-sm:mt-6" onSubmit={handleSubmit}>
                             <div className="-mx-2 md:items-center md:flex mb-16">
-                                {/* <CompanyName value={companyName} onChange={(e) => setCompanyName(e.target.value)} /> */}
-                                <div className="flex-1 px-2 max-sm:mb-5">
-                                    <div className="relative h-11 w-full min-w-[200px]">
-                                        <input placeholder="" name="name" type="text" 
-                                            value={companyName} onChange={(e) => setCompanyName(e.target.value)}
-                                            className="text-white peer h-full w-full border-b border-blue-white bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-white outline outline-0 transition-all placeholder-shown:border-blue-white focus:border-white focus:outline-0 disabled:border-0 disabled:bg-blue-whiteplaceholder:opacity-0 focus:placeholder:opacity-100 " />
-                                        <label
-                                            className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] leading-tight text-white transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 font-normal">
-                                            Nome de l'entreprise <span className={'text-cyan-400 ml-1'}>*</span>
-                                        </label>
-                                    </div>
-                                </div>
+                                <CompanyName value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
                                 <EmailAddress value={email} onChange={(e) => setEmail(e.target.value)} />
                             </div>
                             <div className="-mx-2 md:items-center md:flex mt-16 flex max-sm:gap-1 max-sm:flex-col">
--- src/components/Formation-Courte/DivisContact/formInputs/Formation.jsx
+++ src/components/Formation-Courte/DivisContact/formInputs/Formation.jsx
@@ -9,10 +9,13 @@ function Formation({ value, onChange }) {
                     className="text-white peer h-full w-full border-b border-blue-white bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-white outline outline-0 transition-all placeholder-shown:border-blue-white "
                     value={value} 
                     onChange={onChange}>
-                    <option className='bg-[#071323]' selected>Sélectionner</option>
                     <option className='bg-[#071323]' value="Postman">Postman</option>
                     <option className='bg-[#071323]' value="Cypress">Cypress</option>
                     <option className='bg-[#071323]' value="Selenium">Selenium</option>
+                    <option className='bg-[#071323]' value="Selenium">Git</option>
+                    <option className='bg-[#071323]' value="Selenium">Robot Framework</option>
+                    <option className='bg-[#071323]' value="Selenium">Test Mobile</option>
+                    <option className='bg-[#071323]' value="Selenium">Playwright</option>
                 </select>
                 <label
                     className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] leading-tight text-white transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-white after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-white peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-white peer-focus:after:scale-x-100 peer-focus:after:border-white peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-white font-semibold">
