171dcf17bb7d3e5800e34059fe0fb32af9e353e9
OSMaben <freegiftcardishere@gmail.com>
2024-10-23 23:58:56 +0100

    editing on the section  of our team  on the page of school ,  changing  the  text as well  as removeing a card and leaving only  two  each one contains two images of the team,  and  changing also on section  of  our partners

--- src/components/School/TestemonialSchool.jsx
+++ src/components/School/TestemonialSchool.jsx
@@ -3,8 +3,10 @@ import CommentOfYassineAllami from "../Home/User-Comments/CommentOfMougonFredy.j
 import CommentOfAnasBoukhari from "../Home/User-Comments/CommentOfKemalCherifi.jsx"
 import '../../assets/css/style.css'
 import FirstImage from '../../assets/logos/testimonials.png';
-import SecondImage from './testimonialsImages/SecondImage.jsx';
-import ThirdImage from  './testimonialsImages/ThirdImage.jsx';
+import FourthImage from '../../assets/images/Samira.png';
+import ThirdImage from  '../../assets/images/mimoun.png';
+import SecondImage from "../../assets/images/ali.png";
+
 
 export default function TestemonialSchool(){
 
@@ -15,17 +17,29 @@ export default function TestemonialSchool(){
                 <div className="Comment_title flex justify-center  w-full  h-[35%] mb-10 max-sm:!px-[2%]">
                     <p className="text-white w-8/12 text-4xl text-center font-bold mt-16 max-sm:w-full  max-sm:h-fit max-sm:text-3xl max-sm:!mx-0">Notre équipe aux petits soins de<span className="text-cyan-400 text-center "> Qualité </span></p>
                 </div>
-                <div className="testimonials mt-8 grid grid-cols-1 gap-4 md:grid-cols-3  md:gap-8 testimonialsContainer">
+                <div className="testimonials mt-8 grid grid-cols-1 gap-4 md:grid-cols-2  md:gap-8 testimonialsContainer">
                 <blockquote className="rounded-3xl bg-gray-50 p-[1.7rem] shadow-sm sm:p-[1.7rem]">
-                    <div className="flex items-center gap-4  max-sm:flex-wrap ">
+                    <div className='d-flex'>
+                        <div className="flex items-center gap-4  max-sm:flex-wrap mb-4">
+                            <div className="testimonials_image_profile small_image_container">
+                                {/* <FirstImage /> */}
+                                <img src={FourthImage} className='w-[100%]' alt="" />
+                            </div>
+                            <div className="loading-4">
+                                <p className="mt-0.5 text-lg font-medium text-gray-900">Samira Ladghami</p>
+                                <span className='mt-[-4rem]'>Consultante Administratif</span>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-4  max-sm:flex-wrap ">
                         <div className="testimonials_image_profile small_image_container">
                             {/* <FirstImage /> */}
-                            <img src={FirstImage} alt="" />
+                            <img src={FirstImage} className='w-[100%]' alt="" />
                         </div>
                         <div className="loading-4">
                             <p className="mt-0.5 text-lg font-medium text-gray-900">Asmaa BENNOUF</p>
-                            <span className='mt-[-4rem]'>gestionnaire RH formation</span>
+                            <span className='mt-[-4rem]'>Gestionnaire RH Formation</span>
                         </div>
+                    </div>
                     </div>
 
                     <p className="mt-4 text-gray-700 text-content text-justify">
@@ -33,37 +47,35 @@ export default function TestemonialSchool(){
                     </p>
                 </blockquote>
 
-                <blockquote className="rounded-3xl bg-gray-50 p-[1.7rem] shadow-sm sm:p-[1.7rem]">
-                    <div className="flex items-center gap-4  max-md:flex-wrap">
+
+                <blockquote className="rounded-3xl bg-gray-50 p-[1.7rem] shadow-sm sm:p-[1.7rem] h-[30rem] overflow-scroll">
+                    <div className='d-flex'>
+                        <div className="flex items-center gap-4  max-sm:flex-wrap mb-4">
+                            <div className="testimonials_image_profile small_image_container">
+                                {/* <FirstImage /> */}
+                                <img src={ThirdImage} className='w-[100%]' alt="" />
+                            </div>
+                            <div className="loading-4">
+                                <p className="mt-0.5 text-lg font-medium text-gray-900">Mimoun Kissi</p>
+                                <span className='mt-[-4rem]'>Automation Test Engineer</span>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-4  max-sm:flex-wrap ">
                         <div className="testimonials_image_profile small_image_container">
-                            <SecondImage/>
+                            {/* <FirstImage /> */}
+                            <img src={SecondImage} className='w-[100%]' alt="" />
                         </div>
-
-                        <div>
-                            <p className="mt-0.5 text-lg font-medium text-gray-900">Samira Ladghami</p>
-                            <span className='mt-[-4rem]'>Gestionnaire formation</span>
-
+                        <div className="loading-4">
+                            <p className="mt-0.5 text-lg font-medium text-gray-900">Mohammed Ali EL MALHOUFF</p>
+                            <span className='mt-[-4rem]'>QA Automation Engineer</span>
                         </div>
+                    </div>
                     </div>
 
                     <p className="mt-4 text-gray-700 text-content text-justify">
-                    Les référents admissions sont là pour accompagner les apprenants dans chacune de leurs démarches administratives, et répondre à leurs questions sur les formations. Ils sont donc présents avant même que la formation n’ait commencé. Ce sont aussi eux qui remuent ciel et terre pour faire en sorte que chacun ait les bonnes cartes en main pour décrocher une solution de financement.
-                    </p>
-                </blockquote>
-
-                <blockquote className="rounded-3xl bg-gray-50 p-[1.7rem] shadow-sm sm:p-[1.7rem]">
-                    <div className="flex items-center gap-4  max-md:flex-wrap">
-                        <div className="testimonials_image_profile small_image_container">
-                        <ThirdImage />
-                        </div>
-
-                        <div>
-                            <p className="mt-0.5 text-lg font-medium text-gray-900">Les référents admissions</p>
-                        </div>
-                    </div>
-
-                    <p className="mt-4 text-gray-700 text-content text-justify">
-                    Les référents admissions sont là pour accompagner les apprenants dans chacune de leurs démarches administratives, et répondre à leurs questions sur les formations. Ils sont donc présents avant même que la formation n’ait commencé. Ce sont aussi eux qui remuent ciel et terre pour faire en sorte que chacun ait les bonnes cartes en main pour décrocher une solution de financement.
+                    Notre équipe réunit des professionnels passionnés, Nous nous engageons à concevoir des expériences d'apprentissage innovantes, engageantes et personnalisées.
+Nous collaborons de manière étroite et cohérente, en combinant nos forces pour créer des programmes qui ne se limitent pas à transmettre des connaissances, mais qui motivent les apprenants à se dépasser. Que ce soit à travers des modules interactifs, des outils numériques immersifs ou des méthodes pédagogiques innovantes, nous veillons à ce que chaque expérience soit non seulement éducative mais aussi inspirante.
+Notre objectif est de rendre l’apprentissage accessible à tous, de le dynamiser grâce à des outils modernes, et surtout, d’en faire une expérience plaisante et enrichissante, où chaque apprenant se sent soutenu et valorisé dans sa progression.
                     </p>
                 </blockquote>
 
